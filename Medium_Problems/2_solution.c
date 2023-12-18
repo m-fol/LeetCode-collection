e23fd6793d83ac6b318c9d81952e2b7c2e7e6718
Mary Fol <139060140+m-fol@users.noreply.github.com>
2023-12-18 18:31:04 +0200

    added problem 2 solution

---
+++ Medium_Problems/2_solution.c
@@ -0,0 +1,52 @@
+/*
+2. Add Two Numbers
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+*/
+
+
+/*struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};*/
+
+ 
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            if(current->val <= 9 && current->val >= 0){
+                int sum = carry;
+
+                if (l1 != nullptr) {
+                    sum += l1->val;
+                    l1 = l1->next;
+                }
+
+                if (l2 != nullptr) {
+                    sum += l2->val;
+                    l2 = l2->next;
+                }
+
+                carry = sum / 10;
+                sum %= 10;
+
+                current->next = new ListNode(sum);
+                current = current->next;
+        }
+
+        }
+
+    return dummy->next;
+ }
+};
